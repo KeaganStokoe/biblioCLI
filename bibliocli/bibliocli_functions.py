a1b978c4d53a15c37c8a9526f8f7eb59b5ab2429
Keagan-EXPLORE <keagan@explore-ai.net>
2022-01-18 08:19:51 +0200

    initial commit of core functionality

---
+++ bibliocli/bibliocli_functions.py
@@ -0,0 +1,76 @@
+import click
+import requests
+import json
+from dotenv import load_dotenv
+import os
+import urllib.parse
+
+from helper_functions import mapNotionResultToBook
+
+load_dotenv()
+
+token = os.environ["NOTION_KEY"]
+DATABASE_ID = os.environ["NOTION_DATABASE_ID"]
+NOTION_URL = 'https://api.notion.com/v1/databases/'
+
+def addBookToDatabase(name, author, completed=False):
+    url = f'https://api.notion.com/v1/pages'
+
+    payload = {
+    "parent": {
+        "database_id": DATABASE_ID
+    },
+    "properties": {
+        "Name": {
+        "title": [
+            {
+            "text": {
+                "content": name
+            }
+            }
+        ]
+        },
+        "Author": {
+        "rich_text": [
+            {
+            "text": {
+                "content": author
+            }
+            }
+        ]
+        },
+        "Goodreads link": {
+            "url": urllib.parse.quote(f'https://www.goodreads.com/search?utf8&query={name}', safe=';/?:@&=+$,')
+        },
+        "Completed": {
+        "checkbox": completed
+        }
+    }}
+
+    response = requests.post(url, headers={
+    "Authorization": f"Bearer {token}",
+    "Notion-Version": "2021-08-16",
+    "Content-Type": "application/json"
+    }, data=json.dumps(payload))
+    
+    if response.status_code == 200:
+        click.echo(f'Successfully added "{name}" by "{author}" to the database.')
+    else:
+        click.echo(f'Failed to add "{name}" by "{author}" to the database.') 
+        click.echo(f"Here's the error message: {response.json()}")
+    
+def getBooks():
+  url = f'https://api.notion.com/v1/databases/{DATABASE_ID}/query'
+
+  r = requests.post(url, headers={
+    "Authorization": f"Bearer {token}",
+    "Notion-Version": "2021-08-16"
+  })
+
+  result_dict = r.json()
+  book_list_result = result_dict['results']
+  
+  for book in book_list_result:
+      click.echo(mapNotionResultToBook(book))
+
+
---
+++ bibliocli/helper_functions.py
@@ -0,0 +1,30 @@
+def mapNotionResultToBook(result):
+  # you can print result here and check the format of the answer.
+  book_id = result['id']
+  properties = result['properties']
+  author = properties['Author']['rich_text'][0]['text']['content']
+  name = properties['Name']['title'][0]['text']['content']
+  completed = properties['Completed']['checkbox']
+
+  return {
+    'author': author,
+    'name': name,
+    'completed': completed,
+    'book_id': book_id
+  }
+  
+# For each book called by the API ("json_response_books"), reformat it to only return the title, author(s), and publisher. Return default data if any info is missing ("~XXX not available~") ...
+def reformat_search(response):
+    for item in response:
+        try:
+            title = item["volumeInfo"]['title']
+        except KeyError:
+            title = '~Title not available]~'
+        try:
+            author = " & ".join(item["volumeInfo"]['authors'])
+        except KeyError:
+            author = '~Author not available~'
+        try:
+            publisher = item["volumeInfo"]['publisher']
+        except KeyError:
+            publisher = '~Publisher not available~'
