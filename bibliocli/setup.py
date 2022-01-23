550246022b46b7f1985598ac776cb80d1eddbbc8
Keagan-EXPLORE <keagan@explore-ai.net>
2022-01-23 14:34:48 +0200

    update packaging for installation

--- bibliocli/setup.py
+++ bibliocli/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="BiblioCLI",
-    version="0.1.0",
+    version="0.2.0",
     description="A command line interface for capturing and storing information about books you want to read",
     url="https://github.com/KeaganStokoe/biblioCLI",
     author="Keagan Stokoe",
